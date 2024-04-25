04e5883455432a3b285ab71812215633ab64a27e
brunoguerra <brunoguerra@hotmail.com>
2024-04-25 09:19:23 -0300

    first commit

---
+++ src/test/java/com/postech/mscustomers/MscustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postech.mscustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
